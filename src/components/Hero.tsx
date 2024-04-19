5df2cad2ef9add420f320d3d53e939eb09009e8f
gokuljs <gokuljs029@gmail.com>
2024-04-19 17:17:57 +0530

    hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">Iphone 15 pro</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,34 @@
+import { navLists } from "../constants";
+import { appleImg, bagImg, searchImg } from "../utils";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img
+          src={appleImg}
+          className="justify-start flex"
+          alt="apple"
+          width={14}
+          height={18}
+        />
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((item) => (
+            <div
+              className="px-5 cursor-pointer text-gray hover:text-white transition-all ease-in-out"
+              key={item}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="search" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
